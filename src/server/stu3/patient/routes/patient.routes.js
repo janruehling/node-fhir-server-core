c0eb051472a928ff461a51068dc5cba5fe2866c4
Jon Lee <jlee@asymmetrik.com>
2018-03-21 11:51:26 -0400

    Initial boilerplate for stu3

---
+++ src/server/stu3/patient/routes/patient.routes.js
@@ -0,0 +1,34 @@
+const cors = require('cors');
+const { sanitizeMiddleware } = require('../../../utils/sanitize.utils');
+const { validate } = require('../../../utils/auth');
+const { routes } = require('../patient.config');
+
+/**
+ * @name exports
+ * @summary Patient routes
+ */
+module.exports = (app, config, logger) => {
+	let { profiles, server } = config;
+	let patientProfile = profiles.stu3 && profiles.stu3.patient;
+
+	// Only add routes if we have a patient profile
+	// the endpoint can't function without the config
+	if (patientProfile) {
+		let baseOptions = Object.assign({}, server.corsOptions, patientProfile.corsOptions);
+
+		routes.forEach((route) => {
+			let corsOptions = Object.assign({}, baseOptions, route.corsOptions);
+			// Enable options
+			app.options(route.path, cors(corsOptions));
+			// Enable route
+			app[route.type](
+				route.path,
+				cors(corsOptions),
+				sanitizeMiddleware(route.args),
+				validate(route.scopes, logger, config),
+				route.controller(patientProfile, logger, config)
+			);
+		});
+	}
+
+};
